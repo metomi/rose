50883c75382fe9961b08c8096dc57ff69fe73675
Andrew Clark <andrew.clark@metoffice.gov.uk>
2013-01-25 12:30:44 +0000

    Fixed a typo.

--- lib/python/rosie/browser/result.py
+++ lib/python/rosie/browser/result.py
@@ -142,13 +142,13 @@ class DisplayBox(gtk.VBox):
         return idx, branch, revision
 
     def _get_treeview_path_owner(self, path):
-        """Get the status of a suite"""
+        """Get the owner of a suite"""
         model = self.treeview.get_model()
         i = self.get_column_index_by_name("owner")
         if i is None:
             return False
         return model.get_value(model.get_iter(path), i)
-        
+
     def _get_treeview_path_status(self, path):
         """Get the status of a suite"""
         model = self.treeview.get_model()
