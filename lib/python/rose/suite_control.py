ea1d43f1c0c53a45ad7a87901349f561fa413eb9
Andrew Clark <andrew.clark@metoffice.gov.uk>
2013-05-13 15:36:03 +0100

    Allow sgc to be launched from within a suite sub-dir.

--- lib/python/rose/suite_control.py
+++ lib/python/rose/suite_control.py
@@ -21,6 +21,7 @@
 
 import os
 import rose.config
+from rose.fs_util import FileSystemUtil
 from rose.host_select import HostSelector
 from rose.opt_parse import RoseOptionParser
 from rose.popen import RosePopener
@@ -128,13 +129,37 @@ class SuiteControl(object):
             return conf.get_value(["env", key])
 
 
+class SuiteNotFoundError(Exception):
+
+    """An exception raised when a suite can't be found at or below cwd."""
+    def __str__(self):
+        return ("%s - no suite found for this path." % self.args[0])
+
+
+def get_suite_name(event_handler=None):
+    """Find the top level of a suite directory structure"""
+    fs_util = FileSystemUtil(event_handler)
+    suite_name = None
+    init_dir = os.getcwd()
+    conf_dir = os.getcwd()
+    while True:
+        conf = os.path.join(conf_dir,'rose-suite.conf')
+        if os.path.exists(conf):
+            suite_name = os.path.basename(conf_dir)
+            break
+        else:
+            if not fs_util.dirname(conf_dir) == '/':
+                conf_dir = fs_util.dirname(conf_dir)
+            else:
+                raise SuiteNotFoundError(init_dir)
+    return suite_name
+
 def prompt(action, suite_name, host):
     """Prompt user to confirm action for suite_name at host."""
     if not host:
         host = "localhost"
     return raw_input(PROMPT % (action, suite_name, host)).strip() in [YES]
-
-
+        
 def main():
     """Implement "rose suite-gcontrol" and "rose suite-shutdown"."""
     argv = sys.argv[1:]
@@ -158,7 +183,13 @@ def main():
         if opts.name:
             suite_names.append(opts.name)
         else:
-            suite_names.append(os.path.basename(os.getcwd()))
+            try:
+                suite_name = get_suite_name(event_handler)
+                suite_names.append(suite_name)
+            except SuiteNotFoundError as e:
+                event_handler(e)
+                sys.exit(1)
+
     if opts.debug_mode:
         for sname in suite_names:
             method(sname, opts.host, confirm, sys.stderr, sys.stdout, *args)
@@ -170,7 +201,7 @@ def main():
             except Exception as e:
                 event_handler(e)
                 sys.exit(1)
-        
+
 
 if __name__ == "__main__":
     main()
