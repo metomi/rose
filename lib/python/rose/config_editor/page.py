74012eda0e453b41302589e05b197f0225f1dc31
benfitzpatrick <ben.fitzpatrick@metoffice.gov.uk>
2014-01-06 12:56:38 +0000

    metomi/rose#580: fix file page bug

--- lib/python/rose/config_editor/page.py
+++ lib/python/rose/config_editor/page.py
@@ -664,9 +664,13 @@ class ConfigPage(gtk.VBox):
         std_table = rose.config_editor.pagewidget.table.PageTable
         file_chooser = rose.config_editor.pagewidget.chooser.PageFormatTree
         disc_table = rose.config_editor.pagewidget.table.PageLatentTable
+        ok_file_variable_names = [rose.FILE_VAR_CHECKSUM, rose.FILE_VAR_MODE,
+                                  rose.FILE_VAR_SOURCE]
+        variable_names = [v.name for v in self.panel_data + self.ghost_data]
         if (len(self.sections) == 1 and
-            self.sections[0].name.startswith('file:')):
-            # This page only contains a file section.
+                self.sections[0].name.startswith('file:') and
+                (set(variable_names).issubset(ok_file_variable_names))):
+            # This page only contains a file section and file variables.
             self.main_container = file_chooser(
                                        self.panel_data,
                                        self.ghost_data,
