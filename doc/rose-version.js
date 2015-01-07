12af12b8aa5529b85ab3347df6657ce11a1dc1de
Matt Shin <matthew.shin@metoffice.gov.uk>
2015-01-07 12:14:00 +0000

    Update CHANGES.md for release

--- doc/rose-version.js
+++ doc/rose-version.js
@@ -1,5 +1,5 @@
 /******************************************************************************
- * (C) British crown copyright 2012-4 Met Office.
+ * (C) British crown copyright 2012-5 Met Office.
  *
  * This file is part of Rose, a framework for scientific suites.
  *
@@ -18,4 +18,4 @@
  *
  ******************************************************************************/
 /* N.B. The following line is also used by a shell script. Do not add spaces. */
-ROSE_VERSION="2014.12.0";
+ROSE_VERSION="2015.01.0";
