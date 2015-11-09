8635c7c743176d95f97f558a997ec2daf1b7d1ad
Steve Wardle <steven.wardle@metoffice.gov.uk>
2015-11-09 14:21:29 +0000

    Modify database location (to exclude it from the mirroring)

--- lib/python/rose/apps/rose_ana.py
+++ lib/python/rose/apps/rose_ana.py
@@ -58,7 +58,7 @@ class KGODatabase(object):
     def __init__(self):
         """Initialise the object"""
         self.file_name = os.path.join(
-            os.getenv("ROSE_SUITE_DIR"), "rose-ana-comparisons.db")
+            os.getenv("ROSE_SUITE_DIR"), "log", "rose-ana-comparisons.db")
         self.conn = None
         self.create()
 
