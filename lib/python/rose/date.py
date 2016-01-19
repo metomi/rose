47e649d470e6ee9ba8a4319c985fbfed9fcb1f79
Oliver sanders <oliver.sanders@metoffice.gov.uk>
2016-01-19 12:47:35 +0000

    Implementation of issue #1575 (enhancement request)

--- lib/python/rose/date.py
+++ lib/python/rose/date.py
@@ -304,6 +304,8 @@ def main():
         "parse_format",
         "print_format",
         "task_cycle_time_mode",
+        "new_setting",
+        "as_total",
         "utc_mode")
     opts, args = opt_parser.parse_args()
     report = Reporter(opts.verbosity - opts.quietness)
@@ -328,7 +330,10 @@ def main():
 
     try:
         if len(args) < 2:
-            _print_time_point(date_time_oper, opts, args)
+            if opts.duration_print_format:
+                _convert_duration(date_time_oper, opts, args)
+            else:
+                _print_time_point(date_time_oper, opts, args)
         else:
             _print_duration(date_time_oper, opts, args)
     except OffsetValueError as exc:
@@ -369,6 +374,21 @@ def _print_duration(date_time_oper, opts, args):
             time_point_2 = date_time_oper.date_shift(time_point_2, offset)
     duration, sign = date_time_oper.date_diff(time_point_1, time_point_2)
     print date_time_oper.date_diff_format(opts.print_format, duration, sign)
+
+
+def _convert_duration(date_time_oper, opts, args):
+    """ """
+    time_in_8601 = date_time_oper.duration_parser.parse(args[0])
+    time = time_in_8601.get_seconds()
+
+    options = {'S': time, 'M': time/60, 'H': time/3600}
+    if opts.duration_print_format.upper() in options:
+        # supplied duration format is valid (upper removes case-sentisitivity)
+        print options[opts.duration_print_format.upper()]
+    else:
+        # supplied duration format not valid
+        print 'Invalid date/time format, please use one of h, m, s ' + \
+            '(hours, minutes, seconds)'
 
 
 if __name__ == "__main__":
