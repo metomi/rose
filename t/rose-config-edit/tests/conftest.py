9528abc6158a94e7efd1cb48d98cd216683069a8
Scott Wales <scott.wales@unimelb.edu.au>
2016-07-04 11:01:23 +1000

    Move tests to TAP directory
    Use pytest-tap to output in TAP format

--- t/rose-config-edit/tests/conftest.py
+++ t/rose-config-edit/tests/conftest.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import sys, os
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0,os.path.join(cwd,'../lib/python'))
+sys.path.insert(0,os.path.join(cwd,'../../../lib/python'))
 
 os.environ['ROSE_NS']="namespace"
 os.environ['ROSE_UTIL']="util"
