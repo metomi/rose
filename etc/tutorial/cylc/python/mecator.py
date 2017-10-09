e3ec0635ee6f376745ade2d5cf1f9163b5533fbb
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2017-10-09 10:36:42 +0100

    Added header text.

--- etc/tutorial/cylc/python/mecator.py
+++ etc/tutorial/cylc/python/mecator.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2012-7 Met Office - GNU V3+.
+# This is illustrative code developed for tutorial purposes, it is not
+# intended for scientific use and is not guarantied to be accurate or correct.
+# -----------------------------------------------------------------------------
 import math
 
 
