7f6f2f9f9b68c43facbbad8794e6288914097869
Matt Shin <matthew.shin@metoffice.gov.uk>
2018-01-17 14:08:55 +0000

    rose macro: fix transform opt with arg

---
+++ t/rose-macro/lib/custom_macro_change_arg.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2012-8 Met Office.
+#
+# This file is part of Rose, a framework for meteorological suites.
+#
+# Rose is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Rose is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rose. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+from rose.macro import MacroBase
+
+
+class ArgumentTransformer(MacroBase):
+
+    """Test class to change a setting to a user defined value."""
+
+    def transform(self, config, meta_config=None, myvalue=None):
+        """Perform the transform operation on the env switch."""
+        keys = ["env", "MY_VALUE"]
+        if config.get(keys) is not None:
+            value = config.get(keys).value
+            if value != myvalue:
+                config.set(keys, myvalue)
+                self.add_report(
+                    "env", "MY_VALUE", value, '%s -> %s' % (value, myvalue))
+        return config, self.reports
