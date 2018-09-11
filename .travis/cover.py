d8ca10ebe3e4f7e04431b1c80d2f358cf83a7515
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-09-11 23:08:08 +1200

    Add coverage to project reports.

---
+++ .travis/cover.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2012-8 Met Office.
+#
+# This file is part of Rose, a framework for meteorological suites.
+#
+# Rose is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Rose is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rose. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+import sys
+
+from subprocess import call
+
+
+def main():
+    command = ['rose', 'test-battery', '-j', '5']
+    if call(command + ['--state=save']):
+        # Non-zero return code
+        sys.stderr.write('\n\nRerunning Failed Tests...\n\n')
+        # Exit with final return code
+        sys.exit(call(command + ['--state=save,failed', '-v']))
+
+
+if __name__ == '__main__':
+    main()
