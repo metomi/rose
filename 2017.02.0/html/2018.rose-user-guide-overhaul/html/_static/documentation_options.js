6064c60d196b546a9d2b04cc7bf92b25e7658c35
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2019-06-12 16:49:49 +0100

    remove: 2017.02.0

--- 2017.02.0/html/2018.rose-user-guide-overhaul/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '2018.rose-user-guide-overhaul',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
