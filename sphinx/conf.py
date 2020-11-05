a22660b1a2ac0b7471bd32c44d3154cde5fd6b41
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2020-11-05 08:47:07 +0000

    comment out troublesome hieroglyph theme import

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -55,7 +55,7 @@ extensions = [
     'cylc.sphinx_ext.diff_selection',
     'cylc.sphinx_ext.grid_table',
     'cylc.sphinx_ext.hieroglyph_patch',
-    'cylc.sphinx_ext.hieroglyph_theme_addons',
+    # 'cylc.sphinx_ext.hieroglyph_theme_addons',
     'cylc.sphinx_ext.minicylc',
     'cylc.sphinx_ext.practical',
     'cylc.sphinx_ext.rtd_theme_addons',
