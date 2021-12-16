94de6de0a844af90474ec303716d1c10eff30828
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2021-12-16 08:46:09 +0000

    setup: convert setup.py -> setup.cfg (#2525)
    * Convert setup file format to setup.cfg.
    * Add pygraphviz opt dep.
    * Add logic to metomi.rose.rose to provide more helpful error messages
      when optional dependencies are not present.
    * Add graphviz to CI deps.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 #!/usr/ bin/env python3
-# coding=utf-8
-
 # Copyright (C) British Crown (Met Office) & Contributors.
 #
 # This file is part of Rose, a framework for meteorological suites.
@@ -18,76 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Rose. If not, see <http://www.gnu.org/licenses/>.
 
-import codecs
-from os.path import abspath, dirname, join
-import re
+from setuptools import setup
 
-from setuptools import find_namespace_packages, setup
-
-here = abspath(dirname(__file__))
-
-
-def read(*parts):
-    with codecs.open(join(here, *parts), "r") as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M
-    )
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
-INSTALL_REQUIRES = [
-    "aiofiles",
-    "jinja2>=2.10.1",
-    "ldap3",
-    "metomi-isodatetime",
-    "requests",
-    "sqlalchemy",
-    # TODO: rosie disco has been disabled due to the removal of WSGI support at
-    # Tornado 6.
-    # "tornado",
-    'psutil>=5.6.0',
-]
-EXTRAS_REQUIRE = {
-    'docs': [
-        'sphinx',
-        'sphinx_rtd_theme',
-        'sphinxcontrib-httpdomain',
-        'hieroglyph>=2.1.0',
-        'sphinxcontrib-svg2pdfconverter',
-        'cylc-sphinx-extensions[all]>=1.2.0',
-    ]
-}
-TESTS_REQUIRE = [
-    'pytest',
-    'flake8>=3.0.0',
-    'mypy>=0.800',
-    'types-aiofiles',
-]
-EXTRAS_REQUIRE['all'] = list(
-    set([y for x in EXTRAS_REQUIRE.values() for y in x] + TESTS_REQUIRE)
-)
-
-
-setup(
-    # Metadata
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    version=find_version("metomi", "rose", "__init__.py"),
-    # Options
-    install_requires=INSTALL_REQUIRES,
-    extras_require=EXTRAS_REQUIRE,
-    tests_require=TESTS_REQUIRE,
-    include_package_data=True,
-    packages=find_namespace_packages(include=["metomi.*"]),
-)
+setup()
