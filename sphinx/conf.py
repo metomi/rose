d5df0f71ab262517b65cd04725d129b0d78758ed
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2026-02-19 09:23:48 +0000

    generalized rule not trying to linkcheck linux.die.net

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -224,7 +224,7 @@ texinfo_documents = [
 
 linkcheck_ignore = [
     # linux.die.net doesn't like our request headers
-    'https?://linux.die.net/man/1/rsync',
+    'https?://linux.die.net/*',
     'https?://stackoverflow.com.*',
     # Seems to be too slow, thus causing test failures:
     'https?://www.gnu.org*',
